990811b644ddac4f54d78a04bf506e6ef07a314d
jaylenchan <jaylen.work@hotmail.com>
2024-08-02 18:26:20 +0800

    feat: leetcode

---
+++ ts/leetcode/top-interview-150/array-or-string/best-time-to-buy-and-sell-stock-ii/index.ts
@@ -0,0 +1,38 @@
+/*
+ * @lc app=leetcode.cn id=122 lang=typescript
+ * @lcpr version=30204
+ *
+ * [122] 买卖股票的最佳时机 II
+ */
+
+// @lcpr-template-start
+
+// @lcpr-template-end
+// @lc code=start
+function maxProfit(prices: number[]): number {
+  let profit = 0;
+
+  for (let i = 1; i < prices.length; i++) {
+    if (prices[i] > prices[i - 1]) {
+      profit += prices[i] - prices[i - 1];
+    }
+  };
+
+  return profit
+}
+// @lc code=end
+
+/*
+// @lcpr case=start
+// [7,1,5,3,6,4]\n
+// @lcpr case=end
+
+// @lcpr case=start
+// [1,2,3,4,5]\n
+// @lcpr case=end
+
+// @lcpr case=start
+// [7,6,4,3,1]\n
+// @lcpr case=end
+
+ */
